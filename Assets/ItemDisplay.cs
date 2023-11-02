31b6b6872d9ab460446042335d50201ad14fb04f
IDKBOX <98801077+IDKBOX@users.noreply.github.com>
2023-11-02 23:25:44 +0700

    Add Item Display

---
+++ Assets/ItemDisplay.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class ItemDisplay : MonoBehaviour
+{
+    public int itemsToShow;
+
+    [Header("Prerequisites")]
+    public GameObject item1;
+    public GameObject item2;
+    public GameObject item3;
+
+    void Update()
+    {
+        switch (itemsToShow)
+        {
+            default:
+                item1.SetActive(false);
+                item2.SetActive(false);
+                item3.SetActive(false);
+                break;
+            case 1:
+                item1.SetActive(true);
+                item2.SetActive(false);
+                item3.SetActive(false);
+                break;
+            case 2:
+                item1.SetActive(true);
+                item2.SetActive(true);
+                item3.SetActive(false);
+                break;
+            case 3:
+                item1.SetActive(true);
+                item2.SetActive(true);
+                item3.SetActive(true);
+                break;
+        }
+    }
+}

4e223fed45640f54f218dcdb460deec2892e3e63
IDKBOX <98801077+IDKBOX@users.noreply.github.com>
2023-11-05 10:21:56 +0700

    Add Temporary Orb Collect Particles

--- Assets/Scripts/LevelAssets/SpiritOrb.cs
+++ Assets/Scripts/LevelAssets/SpiritOrb.cs
@@ -3,13 +3,24 @@ using UnityEngine;
 public class SpiritOrb : MonoBehaviour
 {
     public SpiritGate linkedSpiritGate;
+    private ParticleSystem collectEffect;
+
+    private void Awake()
+    {
+        collectEffect = GetComponentInChildren<ParticleSystem>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             linkedSpiritGate.spiritOrbsRequired--;
-            Destroy(gameObject);
+            collectEffect.Play();
+
+            //destroy when touched
+            Destroy(GetComponentInChildren<SpriteRenderer>().gameObject);
+            Destroy(GetComponent<CircleCollider2D>());
+            Destroy(gameObject, 3f);
         }
     }
 }

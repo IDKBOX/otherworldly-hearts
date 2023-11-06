bba789927b6903d3a797c8a208aee05729f5c962
IDKBOX <98801077+IDKBOX@users.noreply.github.com>
2023-11-06 19:06:41 +0700

    Add Story Item UI Animation

--- Assets/StoryItem.cs
+++ Assets/StoryItem.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 public class StoryItem : MonoBehaviour
 {
     public int ItemID;
+    [Header("Prerequisites")]
     public GameObject ItemFoundUI;
     private ParticleSystem collectEffect;
-    private Animator animator;
+    public Animator itemAnimator;
+    public Animator UIAnimator;
 
     private void Awake()
     {
         collectEffect = GetComponentInChildren<ParticleSystem>();
-        animator = GetComponentInChildren<Animator>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -26,17 +27,23 @@ public class StoryItem : MonoBehaviour
     {
         //when first collected
         collectEffect.Play();
-        animator.SetTrigger("Collected");
+        itemAnimator.SetTrigger("Collected");
         Destroy(GetComponentInChildren<SpriteRenderer>().gameObject, 0.5f);
         Destroy(GetComponent<CircleCollider2D>());
         FindObjectOfType<ItemDisplay>().itemsToShow = ItemID;
         ItemFoundUI.SetActive(true);
-        Time.timeScale = 0.15f;
-
-        //wait then run
+        yield return new WaitForSecondsRealtime(0.15f);
+        Time.timeScale = 0.25f;
         yield return new WaitForSecondsRealtime(4f);
+        UIAnimator.SetTrigger("End");
+        yield return new WaitForSecondsRealtime(1f);
         Time.timeScale = 1f;
+        yield return new WaitForSecondsRealtime(2f);
         ItemFoundUI.SetActive(false);
         Destroy(gameObject, 0.1f);
     }
+
+    //TODO
+    //ability unlock
+    //change UI text
 }

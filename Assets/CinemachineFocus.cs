7b5b42139b5dd4c8cd27fdfae654c2e5f71e4782
IDKBOX <98801077+IDKBOX@users.noreply.github.com>
2023-11-06 19:56:42 +0700

    Add Cinemachine Focus Time

--- Assets/CinemachineFocus.cs
+++ Assets/CinemachineFocus.cs
@@ -1,28 +1,36 @@
+using System.Collections;
 using UnityEngine;
 
 public class CinemachineFocus : MonoBehaviour
 {
-    public GameObject roomCamera;
+    public GameObject previousCamera;
     public GameObject focusCamera;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player") && !other.isTrigger)
         {
-            roomCamera.SetActive(false);
+            previousCamera.SetActive(false);
             focusCamera.SetActive(true);
         }
     }
 
-    public void StartCinemachineFocus()
+    public void StartCinemachineFocus(float _focusTime)
     {
-        roomCamera.SetActive(false);
+        StartCoroutine(TimedFocusCoroutine(_focusTime));
+    }
+
+    public IEnumerator TimedFocusCoroutine(float _focusTime)
+    {
+        previousCamera.SetActive(false);
         focusCamera.SetActive(true);
+        yield return new WaitForSecondsRealtime(_focusTime);
+        EndCinemachineFocus();
     }
 
     public void EndCinemachineFocus()
     {
         focusCamera.SetActive(false);
-        roomCamera.SetActive(true);
+        previousCamera.SetActive(true);
     }
 }

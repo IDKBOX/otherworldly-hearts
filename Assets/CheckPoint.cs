893686a010ff6a4f33c87b8c42dd395a643eaa01
IDKBOX <98801077+IDKBOX@users.noreply.github.com>
2023-11-16 14:20:15 +0700

    Add New Checkpoint Sprite & Animations

--- Assets/CheckPoint.cs
+++ Assets/CheckPoint.cs
@@ -2,10 +2,18 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            animator.SetTrigger("On");
             CheckpointManager.Instance.lastCheckPointPos = transform.position;
             CheckpointManager.Instance.checkpointActive = true;
         }

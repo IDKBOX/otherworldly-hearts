6e4f617a1ba7dccd3d5cc908a8044a07358371a0
IDKBOX <98801077+IDKBOX@users.noreply.github.com>
2023-11-16 11:43:50 +0700

    Add Camera Shake, Disable Player Controls on death, Teleport Ghost to Player on Respawn

--- Assets/RespawnPlayer.cs
+++ Assets/RespawnPlayer.cs
@@ -5,27 +5,35 @@ using UnityEngine.SceneManagement;
 public class RespawnPlayer : MonoBehaviour
 {
     private CheckpointManager gm;
-    
+    private CharacterMovement characterMovement;
+    public GameObject ghost;
+
     void Start()
     {
         gm = GameObject.FindGameObjectWithTag("GM").GetComponent<CheckpointManager>();
         transform.position = gm.lastCheckPointPos;
+        characterMovement = GetComponent<CharacterMovement>();
     }
 
     public void respawnPlayer()
     {
+        CinemachineShake.Instance.ShakeCamera(15, 0.1f);
         StartCoroutine(StartTransition());
     }
 
     IEnumerator StartTransition()
     {
+        characterMovement.isDisabled = true;
+        characterMovement.rb.velocity = Vector2.zero;
         TransitionManager.Instance.StartTransition();
         yield return new WaitForSeconds(1f);
 
         SceneManager.UnloadSceneAsync(gm.sceneActive);
         SceneManager.LoadScene(gm.sceneActive, LoadSceneMode.Additive);
         transform.position = gm.lastCheckPointPos;
+        ghost.transform.position = transform.position;
 
+        characterMovement.isDisabled = false;
         TransitionManager.Instance.EndTransition();
     }
 }

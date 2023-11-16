89a81e2f7a8a6f72d20a5019043555b0320b2f0a
IDKBOX <98801077+IDKBOX@users.noreply.github.com>
2023-11-16 12:06:26 +0700

    Add Player Death Animation

--- Assets/RespawnPlayer.cs
+++ Assets/RespawnPlayer.cs
@@ -6,13 +6,21 @@ public class RespawnPlayer : MonoBehaviour
 {
     private CheckpointManager gm;
     private CharacterMovement characterMovement;
-    public GameObject ghost;
+    private Animator animator;
+
+    public GameObject floatingGhost;
+    public GameObject vortexEffect;
+
+    private void Awake()
+    {
+        characterMovement = GetComponent<CharacterMovement>();
+        animator = GetComponentInChildren<Animator>();
+    }
 
     void Start()
     {
         gm = GameObject.FindGameObjectWithTag("GM").GetComponent<CheckpointManager>();
         transform.position = gm.lastCheckPointPos;
-        characterMovement = GetComponent<CharacterMovement>();
     }
 
     public void respawnPlayer()
@@ -24,16 +32,20 @@ public class RespawnPlayer : MonoBehaviour
     IEnumerator StartTransition()
     {
         characterMovement.isDisabled = true;
+        animator.SetTrigger("Death");
+        Instantiate(vortexEffect, transform.position, Quaternion.identity);
         characterMovement.rb.velocity = Vector2.zero;
+        characterMovement.rb.simulated = false;
         TransitionManager.Instance.StartTransition();
         yield return new WaitForSeconds(1f);
 
         SceneManager.UnloadSceneAsync(gm.sceneActive);
         SceneManager.LoadScene(gm.sceneActive, LoadSceneMode.Additive);
         transform.position = gm.lastCheckPointPos;
-        ghost.transform.position = transform.position;
+        floatingGhost.transform.position = transform.position;
 
         characterMovement.isDisabled = false;
+        characterMovement.rb.simulated = true;
         TransitionManager.Instance.EndTransition();
     }
 }

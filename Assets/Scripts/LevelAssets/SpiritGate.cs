b3eca102ccbaffef9ae00c47982446bf22f6a6b2
IDKBOX <98801077+IDKBOX@users.noreply.github.com>
2023-11-20 16:44:25 +0700

    Adjust Level 3 Layout, Add Horizontal Gate bool to Spirit Gate Script

--- Assets/Scripts/LevelAssets/SpiritGate.cs
+++ Assets/Scripts/LevelAssets/SpiritGate.cs
@@ -1,21 +1,26 @@
 using DG.Tweening;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class SpiritGate : MonoBehaviour
 {
     public int spiritOrbsRequired;
     public float gateMoveRange = 4f;
+    public bool horizontalGate;
     bool tweenPlayed = false;
 
     void Update()
     {
-        if (spiritOrbsRequired == 0)
+        if (spiritOrbsRequired == 0 && !tweenPlayed)
         {
-            if (!tweenPlayed)
+            tweenPlayed = true;
+
+            if (!horizontalGate)
             {
-                tweenPlayed = true;
                 transform.DOMoveY(transform.position.y + gateMoveRange, 0.5f);
+            }
+            else
+            {
+                transform.DOMoveX(transform.position.x + gateMoveRange, 0.5f);
             }
         }
     }

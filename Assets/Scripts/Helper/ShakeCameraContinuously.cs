20543831d54c8bb7bd25dfe35703699807bb537a
IDKBOX <98801077+IDKBOX@users.noreply.github.com>
2023-12-01 14:20:02 +0700

    Add Camera shake to level 4, fix tilemap

--- Assets/Scripts/Helper/ShakeCameraContinuously.cs
+++ Assets/Scripts/Helper/ShakeCameraContinuously.cs
@@ -2,13 +2,34 @@ using UnityEngine;
 
 public class ShakeCameraContinuously : MonoBehaviour
 {
+    public float shakeValue = 5f;
+    public bool enableOnStart;
+    private bool shakeStarted;
+
+    private void Start()
+    {
+        if (enableOnStart)
+        {
+            StartCameraShake();
+        }
+    }
+
+    private void Update()
+    {
+        if (shakeStarted)
+        {
+            StartCameraShake();
+        }
+    }
     public void StartCameraShake()
     {
-        CinemachineShake.Instance.ShakeCamera(5f, float.PositiveInfinity);
+        CinemachineShake.Instance.ShakeCamera(shakeValue, float.PositiveInfinity);
+        shakeStarted = true;
     }
 
     public void StopCameraShake()
     {
         CinemachineShake.Instance.ShakeCamera(0f, 0.1f);
+        shakeStarted= false;
     }
 }
